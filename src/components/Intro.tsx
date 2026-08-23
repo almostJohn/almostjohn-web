f586f9ffc2ae1b5140adbe8d97d30e778ba63d35
almostJohn <garcia.johngale@gmail.com>
2026-08-23 14:27:27 +0800

    update website

---
+++ src/components/Intro.tsx
@@ -0,0 +1,13 @@
+export function Intro() {
+	return (
+		<div className="mt-3 flex flex-col gap-2 md:mt-0">
+			<h1 className="text-2xl font-bold">
+				👋 Hey, I'm <span className="text-blue-500">John</span>!
+			</h1>
+			<p className="text-sm leading-relaxed text-pretty">
+				I love building things, and solving problems. I enjoy language design,
+				programming interfaces, and web development.
+			</p>
+		</div>
+	);
+}
